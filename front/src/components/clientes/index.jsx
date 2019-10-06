feb23d1a14f3e3a2e5115ed447357feb27748701
luizdequeiroz <oluizdequeiroz@gmail.com>
2019-10-06 14:49:56 -0300

    convertendo index de cliente em componente funcional e ajustando para busca de clientes via api dotnet core.

--- front/src/components/clientes/index.jsx
+++ front/src/components/clientes/index.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import clienteTable from './table';
+import React, { useState, useEffect } from 'react';
+import ClienteTable from './table';
 import PesquisarClienteForm from "./pesquisarClienteForm";
 import ClienteForm from "./clienteForm";
 import { bindDefault } from '../../config/binders';
@@ -7,78 +7,58 @@ import { bindDefault } from '../../config/binders';
 import ToolkitProvider from 'react-bootstrap-table2-toolkit';
 import { treatDefault as treatment } from '../../treatments';
 
-class Clientes extends Component {
+import { API_DOTNET } from '../../utils';
 
-    state = {
-        novoCliente: false,
-        visible: false
+function Clientes({ get, cliente, }) {
+    const [novoCliente, setNovoCliente] = useState(false);
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        get('Clientes', 'clientes', { api: API_DOTNET, treatment });
+    }, [get]);
+
+    const propsCollapse = {
+        style: (novoCliente || cliente) && { cursor: 'pointer' },
+        onClick: (novoCliente || cliente) && (() => setVisible(!visible))
     };
 
-    componentDidMount() {
-        const { get } = this.props;
-
-        get('cliente', 'clientes', { treatment });
-    }
-
-    cancelar() {
-        const { setValue } = this.props;
-
-        setValue('cliente');
-        setValue('clienteRegistro');
-        this.setNovoCliente(false);
-    }
-
-    setNovoCliente(flag) {
-        this.setState({ novoCliente: flag });
-    }
-
-    render() {
-        const { cliente } = this.props;
-        const { novoCliente } = this.state;
-
-        const propsCollapse = {
-            style: (novoCliente || cliente) && { cursor: 'pointer' },
-            onClick: (novoCliente || cliente) && (() => this.setState({ visible: !this.state.visible }))
-        };
-
-        return (
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="card">
-                            <div className="card-header card-header-primary" {...propsCollapse}>
-                                <h4 className="card-title">Gerenciamento de clientes</h4>
-                            </div>
-                            {(!novoCliente && !cliente) || this.state.visible ?
-                                <div className="card-body">
-                                    <ToolkitProvider search>{props => (
-                                        <div>
-                                            <PesquisarClienteForm key={cliente && cliente.codigo} onNovoCliente={this.setNovoCliente.bind(this)} searchProps={props.searchProps} />
-                                            <div style={{ marginTop: '40px' }}>
-                                                <clienteTable baseProps={props.baseProps} />
-                                            </div>
-                                        </div>
-                                    )}</ToolkitProvider>
-                                </div>
-                                : <div className="card-body" />}
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="card">
+                        <div className="card-header card-header-primary" {...propsCollapse}>
+                            <h4 className="card-title">Gerenciamento de clientes</h4>
                         </div>
+                        {(!novoCliente && !cliente) || visible ?
+                            <div className="card-body">
+                                <ToolkitProvider search>{props => (
+                                    <div>
+                                        <PesquisarClienteForm key={cliente && cliente.id} onNovoCliente={setNovoCliente} searchProps={props.searchProps} />
+                                        <div style={{ marginTop: '40px' }}>
+                                            <ClienteTable baseProps={props.baseProps} />
+                                        </div>
+                                    </div>
+                                )}</ToolkitProvider>
+                            </div>
+                            : <div className="card-body" />}
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <div className="card">
-                            <div className="card-header card-header-primary">
-                                <h4 className="card-title">{(novoCliente || cliente) ? `${novoCliente ? 'Cadastrar' : 'Editar'} cliente` : 'Selecione um cliente acima ou clique em Novo cliente...'}</h4>
-                            </div>
-                            <div className="card-body">
-                                {(novoCliente || cliente) && <ClienteForm key={cliente && cliente.codigo} cancelar={this.cancelar.bind(this)} onNovoCliente={this.setNovoCliente.bind(this)} />}
-                            </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="card">
+                        <div className="card-header card-header-primary">
+                            <h4 className="card-title">{(novoCliente || cliente) ? `${novoCliente ? 'Cadastrar' : 'Editar'} cliente` : 'Selecione um cliente acima ou clique em Novo cliente...'}</h4>
+                        </div>
+                        <div className="card-body">
+                            {(novoCliente || cliente) && <ClienteForm key={cliente && cliente.id} onNovoCliente={flag => setNovoCliente(flag)} />}
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default bindDefault('cliente')(Clientes);
