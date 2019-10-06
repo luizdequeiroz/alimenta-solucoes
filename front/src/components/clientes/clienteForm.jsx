f55d8ed037522f8df0286f65dde86e89e19c2786
luizdequeiroz <oluizdequeiroz@gmail.com>
2019-10-06 14:49:02 -0300

    carregamento de dados de cliente em formulário.

--- front/src/components/clientes/clienteForm.jsx
+++ front/src/components/clientes/clienteForm.jsx
@@ -34,10 +34,29 @@ function validate(values) {
     return errors;
 }
 
-export default bindReduxForm('cliente', 'clienteRegistro')(register)(validate)(({ dispatch, form, cliente, cancelar, handleSubmit, clienteRegistro, get, setValue }) => {
+export default bindReduxForm('cliente', 'clienteRegistro')(register)(validate)(({ dispatch, form, cliente, onNovoCliente, handleSubmit, clienteRegistro, get, setValue }) => {
+
+    function enderecoEntrega(cliente) {        
+        const enderecoEntrega = {};
+        Object.keys(cliente.enderecoEntrega).forEach(key => enderecoEntrega[`${key}Entrega`] = cliente.enderecoEntrega[key]);
+        return enderecoEntrega;
+    }
 
     useEffect(() => {
-        dispatch(initialize(form, cliente));
+        const clienteWC = cliente && {
+            ...cliente,
+            endereco: undefined,
+            ...cliente.endereco,
+            id: cliente.id,
+            idEndereco: cliente.endereco.id,
+            enderecoEntrega: undefined,
+            ...enderecoEntrega(cliente)
+        };
+
+        dispatch(initialize(form, clienteWC));
+    }, [dispatch, form, cliente]);
+
+    useEffect(() => {
         if (clienteRegistro) {
             if (clienteRegistro.sucesso) {
                 get('cliente', 'clientes', { treatment });
@@ -47,11 +66,13 @@ export default bindReduxForm('cliente', 'clienteRegistro')(register)(validate)((
             }
             setValue('clienteRegistro');
         }
-    }, [dispatch, form, cliente, clienteRegistro, get, setValue]);
+    }, [clienteRegistro, get, setValue]);
 
     function voltar() {
-        dispatch(initialize(form, cliente));
-        cancelar();
+
+        setValue('cliente');
+        setValue('clienteRegistro');
+        onNovoCliente(false);
     }
 
     return (
@@ -65,41 +86,60 @@ export default bindReduxForm('cliente', 'clienteRegistro')(register)(validate)((
                         <Accordion.Collapse eventKey="0">
                             <Card.Body>
                                 <Row>
-                                    <Col><Field name="nome" component={Input} type="text" placeholder="Nome da cliente" popoverPosition="top" /></Col>
-                                    <Col><Field name="CPF" component={Input} type="text" placeholder="CPF da cliente" popoverPosition="top" /></Col>
-                                    <Col><Field name="cnpj" component={Input} type="text" placeholder="CNPJ da cliente" popoverPosition="top" /></Col>
-                                   
+                                    <Col><Field name="nome" component={Input} type="text" placeholder="Nome do cliente" popoverPosition="top" /></Col>
+                                    <Col><Field name="cpf" component={Input} type="text" placeholder="CPF do cliente" popoverPosition="top" /></Col>
+                                    <Col><Field name="cnpj" component={Input} type="text" placeholder="CNPJ do cliente" popoverPosition="top" /></Col>
                                 </Row>
                                 <Row>
-                                  <Col><Field name="representante" component={Input} type="text" placeholder="Representante do cliente" popoverPosition="top" /></Col>
-                                  <Col><Field name="valorRefeicao" component={Input} type="text" placeholder="Valor da Refeição" popoverPosition="top" /></Col>
-                                  <Col><Field name="qtdRefeicoes" component={Input} type="text" placeholder="Qtd Refeições" popoverPosition="top" /></Col>
+                                    {/* <Col><Field name="representante" component={Input} type="text" placeholder="Representante do cliente" popoverPosition="top" /></Col> */}
+                                    <Col><Field name="telefone" component={Input} type="text" placeholder="Valor da Refeição" popoverPosition="top" /></Col>
                                 </Row>
-                                <Row>
-                                 <Col><Field name="contrato" component={Input} type="text" placeholder="Contrato do cliente" popoverPosition="top" /></Col>
-                                </Row>
+                                {/* <Row>
+                                    <Col><Field name="contrato" component={Input} type="text" placeholder="Contrato do cliente" popoverPosition="top" /></Col>
+                                </Row> */}
                             </Card.Body>
                         </Accordion.Collapse>
                     </Card>
                     <Card>
                         <Card.Header>
                             <Accordion.Toggle as={Button} variant="link" eventKey="1">
-                               ENDEREÇO DA EMPRESA
+                                ENDEREÇO DA EMPRESA
                             </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="1">
                             <Card.Body>
                                 <Row>
                                     <Col><Field name="logradouro" component={Input} type="text" placeholder="Logradouro da cliente" popoverPosition="top" /></Col>
-                                    <Col><Field name="mumero" component={Input} type="text" placeholder="Nº" popoverPosition="top" /></Col>
+                                    <Col><Field name="numero" component={Input} type="text" placeholder="Nº" popoverPosition="top" /></Col>
                                     <Col><Field name="complemento" component={Input} type="text" placeholder="Complemento" popoverPosition="top" /></Col>
-                                   
                                 </Row>
                                 <Row>
                                     <Col><Field name="bairro" component={Input} type="text" placeholder="Bairro" popoverPosition="top" /></Col>
                                     <Col><Field name="cep" component={Input} type="text" placeholder="CEP" popoverPosition="top" /></Col>
                                     <Col><Field name="cidade" component={Input} type="text" placeholder="Cidade" popoverPosition="top" /></Col>
                                     <Col><Field name="estado" component={Input} type="text" placeholder="Estado" popoverPosition="top" /></Col>
+                                </Row>
+                            </Card.Body>
+                        </Accordion.Collapse>
+                    </Card>
+                    <Card>
+                        <Card.Header>
+                            <Accordion.Toggle as={Button} variant="link" eventKey="2">
+                                ENDEREÇO DE ENTREGA
+                            </Accordion.Toggle>
+                        </Card.Header>
+                        <Accordion.Collapse eventKey="2">
+                            <Card.Body>
+                                <Row>
+                                    <Col><Field name="logradouroEntrega" component={Input} type="text" placeholder="Logradouro de entrega" popoverPosition="top" /></Col>
+                                    <Col><Field name="numeroEntrega" component={Input} type="text" placeholder="Nº de entrega" popoverPosition="top" /></Col>
+                                    <Col><Field name="complementoEntrega" component={Input} type="text" placeholder="Complemento de endereço de entrega" popoverPosition="top" /></Col>
+                                </Row>
+                                <Row>
+                                    <Col><Field name="bairroEntrega" component={Input} type="text" placeholder="Bairro de entrega" popoverPosition="top" /></Col>
+                                    <Col><Field name="cepEntrega" component={Input} type="text" placeholder="CEP de entrega" popoverPosition="top" /></Col>
+                                    <Col><Field name="cidadeEntrega" component={Input} type="text" placeholder="Cidade de entrega" popoverPosition="top" /></Col>
+                                    <Col><Field name="estadoEntrega" component={Input} type="text" placeholder="Estado de entrega" popoverPosition="top" /></Col>
                                 </Row>
                             </Card.Body>
                         </Accordion.Collapse>
